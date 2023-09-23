7e6ae5f87ecee8e96301015e664e999a7ec0066e
Gustavo Ferreira <gustavo.ferreira@nutrip.com.br>
2023-09-23 17:39:58 -0300

    chore: initial business microservice

---
+++ ms-business/src/main/java/br/com/tgid/msbusiness/MsbusinessApplication.java
@@ -0,0 +1,13 @@
+package br.com.tgid.msbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsbusinessApplication.class, args);
+	}
+
+}
